874dca29f7d5662a72e5aea73cf8fdca303ec83a
Arron Kurniawan <arronku2003@gmail.com>
2023-02-18 21:11:00 +0700

    ayam

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,50 @@
+import Engine.Window;
+import org.lwjgl.glfw.GLFW.*;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL30.*;
+
+
+
 public class Main {
+    private Window window =
+            new Window(800,800,"Hello World");
+    public void run() {
+        init();
+        loop();
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+
+    }
+
+    public void init() {
+        window.init();
+        GL.createCapabilities();
+
+        //code taro setelah init dan GL
+    }
+
+    public void loop() {
+//        kalo ga pake loop nnti habis buka window baru langsung ketutup (ga bisa tambahin frame)
+        while(window.isOpen()){
+            window.update();
+//            warna hanya 0 - 1 pake f
+            glClearColor(0,0,0,0);
+            GL.createCapabilities();
+
+            //code
+            //..
+
+            glDisableVertexAttribArray(0);
+            glfwPollEvents();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main().run();
+
     }
 }
